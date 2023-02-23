aac5624c067db04c13c6b449f204cf45c7330aae
mostafa <imoztafa@gmail.com>
2023-02-23 15:53:42 -0800

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "alx.h"
+
+/**
+ * _isupper - check uppercase letter
+ * @c: entered char
+ * Return i
+ */
+int _isupper(int c)
+{
+		if (c >= 65 && c<= 90)
+		{
+				return (i);
+		}
+		else
+		{
+				return (0);
+		}
+}
+
